948631cdf3dc957a10f800054dd5a6736c90d8f9
Riza Fahmi <rizafahmi@gmail.com>
2016-06-11 09:25:29 +0700

    Finish add secret page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule GuardianOfGalaxy.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+  
+  def secret(conn, _params) do
+    render conn, "secret.html"
+  end
 end
